4f3a6a48b0c873efad11d2441d6cceff5f5355d3
Camila Sena Araújo <camilasenaraujo@gmail.com>
2025-05-14 21:22:39 -0300

    ajustes no script de testes do projeto Refund

--- modulo-4/projetos/refund-5/testes.js
+++ modulo-4/projetos/refund-5/testes.js
@@ -1,12 +1,12 @@
 // configurando amount
 
-const amount = document.getElementById("amount")
+const valorDespesa = document.getElementById("amount")
 
-amount.addEventListener("input", () => {
+valorDespesa.addEventListener("input", () => {
   // somente digitos
-  let valorDigito = amount.value.replace(/\D/g,"")
+  let valorDigito = valorDespesa.value.replace(/\D/g,"")
   valorFinal = Number(valorDigito) / 100
-  amount.value = formatCurrencyBRL(valorFinal)
+  valorDespesa.value = formatCurrencyBRL(valorFinal)
   
 })
 
@@ -16,4 +16,35 @@ function formatCurrencyBRL(valor) {
     currency: "BRL"
   })
   return valor
+ }
+
+ // configurando o submit
+
+ const form = document.querySelector("form")
+ const categoria = document.getElementById("category")
+ const despesa = document.getElementById("expense")
+
+ form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  const novaDespesa = {
+    id: new Date().getTime(),
+    despesa: despesa.value,
+    categoria_id: categoria.value,
+    categoria_nome: categoria.options[category.selectedIndex].text,
+    valor: valorDespesa.value,
+    criadoEm: new Date().toLocaleDateString("pt-BR")
+  }
+
+  adicionaDespesa(novaDespesa)
+ })
+
+ function adicionaDespesa(novaDespesa) {
+    try {
+      throw new Error("Erro de teste")
+    } catch (error) {
+      alert("Não foi possível atualizar a lista de despesas")
+      console.log(error)
+    }
+
  }
