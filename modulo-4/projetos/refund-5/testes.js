9c208d8bbd29f16b06aec6f1ac319478636f9ded
Camila Sena Araújo <camilasenaraujo@gmail.com>
2025-05-15 17:37:38 -0300

    atualizando teste do projeto Refund

--- modulo-4/projetos/refund-5/testes.js
+++ modulo-4/projetos/refund-5/testes.js
@@ -1,3 +1,5 @@
+const listaItens = document.querySelector("ul")
+
 // configurando amount
 
 const valorDespesa = document.getElementById("amount")
@@ -29,7 +31,7 @@ function formatCurrencyBRL(valor) {
 
   const novaDespesa = {
     id: new Date().getTime(),
-    despesa: despesa.value,
+    nome: despesa.value,
     categoria_id: categoria.value,
     categoria_nome: categoria.options[category.selectedIndex].text,
     valor: valorDespesa.value,
@@ -41,7 +43,38 @@ function formatCurrencyBRL(valor) {
 
  function adicionaDespesa(novaDespesa) {
     try {
-      throw new Error("Erro de teste")
+      //adiciona novo li
+      const item = document.createElement("li")
+      item.classList.add("expense")
+      // adiciona o icone do li
+      const icon = document.createElement("img")
+      icon.setAttribute('src',`./img/${novaDespesa.categoria_id}.svg`)
+      icon.setAttribute('alt',novaDespesa.categoria_nome)
+
+      //adiciona div com o nome e tipo da despesa
+      const infoDespesa = document.createElement("div")
+      infoDespesa.classList.add("expense-info")
+      const nome = document.createElement("strong")
+      nome.textContent= novaDespesa.nome
+      const categoria = document.createElement("span")
+      categoria.textContent= novaDespesa.categoria_nome
+
+      //adiciona valor
+      const valor = document.createElement("span")
+      valor.classList.add("expense-amount")
+      valor.innerHTML = `<small>R$</small> ${novaDespesa.valor.toUpperCase().replace("R$","")}`
+
+      // adiciona botao de remover
+
+      const botaoRemove = document.createElement("img")
+      botaoRemove.classList.add("remove-icon")
+      botaoRemove.setAttribute("src","./img/remove.svg")
+      botaoRemove.setAttribute("alt","Remover")
+
+      infoDespesa.append(nome,categoria)
+      item.append(icon,infoDespesa,valor,botaoRemove)
+      listaItens.append(item)
+
     } catch (error) {
       alert("Não foi possível atualizar a lista de despesas")
       console.log(error)
