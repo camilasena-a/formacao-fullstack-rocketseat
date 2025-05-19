fd0d400e6a45c0674f87c729bc20ed75a699730d
Camila Sena Araújo <camilasenaraujo@gmail.com>
2025-05-19 20:59:35 -0300

    adição de notas sobre metodo every em arrays

---
+++ modulo-4/javascript-pre-framework/js-moderno/metodos-array/every.js
@@ -0,0 +1,6 @@
+// retorna valor boleeano, testa se todos os elementos do array passam na condição de retorno
+
+const ages = [15,30,29,24]
+
+const result = ages.every((age)=> age>=18)
+console.log(result)
