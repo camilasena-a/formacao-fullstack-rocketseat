2f82dd5b32d4a8b8c3a201eb673f9e316d254588
Camila Sena Araújo <camilasenaraujo@gmail.com>
2025-05-20 18:45:10 -0300

    adição de notas sobre metodo reduce em arrays

--- modulo-4/javascript-pre-framework/js-moderno/metodos-array/reduce.js
+++ modulo-4/javascript-pre-framework/js-moderno/metodos-array/reduce.js
@@ -1,23 +1,51 @@
-/* utilizado para reduzir um array a um unico valor
+/* utilizado para extrair valores únicos dentro de um array, por exemplo: maior numero, menor numero, média dos valores do array, soma dos valores do array, etc
 
 Parametros
 
 - array original
-- acumulador
-- valor da iteracao
-- valor inicial
-- index (index da iteracao atual - opcional)
+
+- A. variavel auxiliar, return retorna novos valores de aux
+- B. valor da iteracao (valor contido no index atual)
+- C. index (index da iteracao atual - opcional)
+
+- D. valor inicial da variavel auxiliar (segundo parametro do reduce, por ex 0, pode ser omitido e, nesse caso, atribui o mesmo valor do primeiro indice)
+
+exemplo:
+
+arrayOriginal.reduce( (A,B,C) => {bloco de código} , D )
 
 */
 
-const values = [1,2,3,4,5]
+const values = [27,10,43,94,85]
 
-const sum = values.reduce((acumulador, valorIteracao, index) => {
-  console.log("ACUMULADOR", acumulador)
-  console.log("VALOR ITERACAO", valorIteracao)
-  console.log("INDEX", index)
-  
-  console.log("SOMA", acumulador+valorIteracao)
-  console.log("########################################")
+// extraindo soma dos valores
 
+const sum = values.reduce((aux, valor, index) => {
+  return aux + valor
+},0)
+
+console.log(sum)
+
+// extraindo maior valor
+
+const maiorValor = values.reduce((aux,valorAtual) => {
+  return aux > valorAtual ? aux : valorAtual
+}, 0)
+
+console.log(maiorValor)
+
+// extraindo menor valor
+
+const menorValor = values.reduce((aux,valorAtual) => {
+  return aux < valorAtual ? aux : valorAtual
 })
+
+console.log(menorValor)
+
+// extraindo media dos valores
+
+const media = values.reduce((aux, valor, index) => {
+  return aux + valor
+},0) / values.length
+
+console.log(media)
