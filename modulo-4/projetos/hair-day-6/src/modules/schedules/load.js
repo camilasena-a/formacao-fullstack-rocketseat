90839cb0ff3072cd8a0c6f0f82c0ddb3485c5b87
Camila Sena <84555271+camilasena-a@users.noreply.github.com>
2025-05-31 16:50:42 -0300

    Update load.js

--- modulo-4/projetos/hair-day-6/src/modules/schedules/load.js
+++ modulo-4/projetos/hair-day-6/src/modules/schedules/load.js
@@ -1,8 +1,13 @@
+import { buscaAgendamento } from "../../services/buscaAgendamento";
 import { hoursLoad } from "../form/hours-load";
 
 const dataSelecionada = document.querySelector("#date")
 
-export function agendamentosDia() {
+export async function agendamentosDia() {
+  // Obtem a data do imput
   const data = dataSelecionada.value
+  // Busca na API os agendamentos do dia
+  const daily = await buscaAgendamento({data})
+  // Renderiza as horas disponiveis
   hoursLoad({data})
 }
