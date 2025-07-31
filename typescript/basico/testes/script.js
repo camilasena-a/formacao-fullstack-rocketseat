b732a90a325e5fc143b6ceb31bb41714c96aeec0
Camila Sena Araújo <camilasenaraujo@gmail.com>
2025-07-31 20:40:20 -0300

    Adiciona arquivos HTML e TypeScript para testes, incluindo estrutura básica de página e exemplos de funções e tipos. Implementa configuração do TypeScript com tsconfig.json e anotações de tipos em variáveis e funções.

---
+++ typescript/basico/testes/script.js
@@ -0,0 +1,16 @@
+"use strict";
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(1, 2));
+// declaração de variáveis
+let produto = "Notebook";
+const preco = 1000;
+//produto = 2 --- vai dar erro pq o produto deve ser uma string
+const carro = {
+    marca: "Ford",
+    modelo: "Mustang",
+    ano: 2024
+};
+// carro.ano = "celta" --- vai dar erro pq o ano deve ser um número
+console.log(carro);
--- typescript/basico/testes/script.ts
+++ typescript/basico/testes/script.ts
@@ -3,3 +3,22 @@ function soma(a: number, b: number) {
 }
 
 console.log(soma(1, 2));
+
+// declaração de variáveis
+
+let produto: string = "Notebook";
+const preco: number = 1000;
+
+//produto = 2 --- vai dar erro pq o produto deve ser uma string
+
+const carro: { marca: string; modelo: string; ano: number } = {
+  marca: "Ford",
+  modelo: "Mustang",
+  ano: 2024
+}
+
+// carro.ano = "celta" --- vai dar erro pq o ano deve ser um número
+
+console.log(carro);
+
+
